25c1a1b3639202f6ac9aa51e51e26f0a7d166fb5
lemical07 <icaleladio@gmail.com>
2026-08-13 20:43:39 -0600

    feat(dql): add v_order_summary_by_office view

--- dql/views_and_queries.sql
+++ dql/views_and_queries.sql
@@ -1,4 +1,16 @@
 USE gaseosas_del_valle;
+
+CREATE VIEW v_order_summary_by_office AS
+SELECT lo.id_loc_office, lo.office_name,
+    COUNT(DISTINCT o.id_order) AS total_orders,
+    SUM(fn_calculate_total_with_iva(o.id_order)) AS total_sales
+FROM location_office lo
+    JOIN orders o 
+    ON o.id_loc_office = lo.id_loc_office
+GROUP BY lo.id_loc_office, lo.office_name;
+SELECT * FROM v_order_summary_by_office;
+
+
 CREATE VIEW v_order_summary AS
 SELECT
     o.id_order,
@@ -8,7 +20,8 @@ SELECT
     o.status,
     fn_calculate_total_with_iva(o.id_order) AS total_with_iva
 FROM orders o
-    JOIN client c ON o.id_client = c.id_client
+    JOIN client c 
+        ON o.id_client = c.id_client
     JOIN location_office lo ON o.id_loc_office = lo.id_loc_office;
-    
+
 SELECT * FROM v_order_summary ORDER BY id_order;
