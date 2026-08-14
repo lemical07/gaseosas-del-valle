a226e43841d6048365923ebb19786944a97abf2a
lemical07 <icaleladio@gmail.com>
2026-08-13 21:07:26 -0600

    fix(dql): clean views_and_queries.sql, remove ad-hoc test statements

--- dql/views_and_queries.sql
+++ dql/views_and_queries.sql
@@ -8,7 +8,6 @@ FROM location_office lo
     JOIN orders o 
     ON o.id_loc_office = lo.id_loc_office
 GROUP BY lo.id_loc_office, lo.office_name;
-SELECT * FROM v_order_summary_by_office;
 
 CREATE VIEW v_products_below_min_stock AS
 SELECT p.id_product, p.name AS product_name,
@@ -21,26 +20,12 @@ FROM stock s
         ON s.id_loc_office = lo.id_loc_office
 WHERE s.current_stock <= s.minimum_stock_level;
 
-SELECT * FROM v_products_below_min_stock;
--- ------before test
-UPDATE stock SET current_stock = 10 WHERE id_product = 1 AND id_loc_office = 1;
-SELECT * FROM v_products_below_min_stock;
--- ----after test
-UPDATE stock SET current_stock = 250 WHERE id_product = 1 AND id_loc_office = 1;
-SELECT COUNT(*) FROM stock WHERE current_stock <= minimum_stock_level;
-
-
 CREATE VIEW v_active_clients AS
 SELECT DISTINCT c.id_client, c.first_name, c.last_name, c.email
 FROM client c
     JOIN orders o 
         ON o.id_client = c.id_client;
 
-SELECT * FROM v_active_clients;
-
-SELECT c.id_client, c.first_name FROM client c
-LEFT JOIN orders o ON o.id_client = c.id_client
-WHERE o.id_order IS NULL;
 CREATE VIEW v_order_summary AS
 SELECT
     o.id_order,
@@ -53,5 +38,3 @@ FROM orders o
     JOIN client c 
         ON o.id_client = c.id_client
     JOIN location_office lo ON o.id_loc_office = lo.id_loc_office;
-
-SELECT * FROM v_order_summary ORDER BY id_order;
