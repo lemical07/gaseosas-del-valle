d97dc1b9a410e93a215f2df6e453699091ec6b00
lemical07 <icaleladio@gmail.com>
2026-08-13 21:20:07 -0600

    feat(dql): add query for best-selling products

--- dql/queries.sql
+++ dql/queries.sql
@@ -14,3 +14,11 @@ SELECT id_order, id_client, id_loc_office, order_date, status
 FROM orders
 WHERE order_date 
     BETWEEN '2026-01-01' AND '2026-02-28';
+
+SELECT p.id_product, p.name AS product_name,
+    SUM(od.quantity) AS total_units_sold
+FROM order_details od
+    JOIN product p 
+        ON od.id_product = p.id_product
+GROUP BY p.id_product, p.name
+ORDER BY total_units_sold DESC;
